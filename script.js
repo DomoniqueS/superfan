8828b783297a39616c16c6635f396e36900474a8
DomoniqueS <stewartdomonique01@gmail.com>
2017-12-12 09:45:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function 2pacldl(elem){
-document.getElementById("video").play();
+!This function will allow the viewer to play music when they click on an image!
+function playsong(e){
+  document.getElementById("badboy").play();
+  document.getElementById("bigpoppa").pause();
+  document.getElementById("hypnotize").pause();
+  document.getElementById("badboy").pause();
+}
+function playsong(e){
+  document.getElementById("bigpoppa").play();
+  document.getElementById("badboy").pause();
+  document.getElementById("hypnotize").pause();
+  document.getElementById("badboy").pause();
+}
+function playsong(e){
+  document.getElementById("hynotize").play();
+  document.getElementById("badboy").pause();
+  document.getElementById("bigpoppa").pause();
+  document.getElementById("").pause();
+}
+function playsong(e){
+  document.getElementById("").play();
+  document.getElementById("badboy").pause();
+  document.getElementById("bigpoppa").pause();
+  document.getElementById("hypnotize").pause();
 }
