25f8c1c5165a21bb398f9c6631cdeaa876fa15c6
DomoniqueS <stewartdomonique01@gmail.com>
2017-12-13 13:11:14 -0800

    Update script.js

--- tupac/script.js
+++ tupac/script.js
@@ -1,6 +1,6 @@
 
 function playsong1(e){
-	!-This code allows you to click on an image and the image will play a song-!
+	<!--This code allows you to click on an image and the image will play a song--!>
 	document.getElementById("badboy").play();
 	document.getElementById("bigpoppa2").pause();
 	document.getElementById("hypnotize").pause();
@@ -9,7 +9,7 @@ function playsong1(e){
 }
 function play2song(e){
 	document.getElementById("bigpoppa2").play();
-	!-This code allows you to click on a song and pause a song thats already playing-!
+	<!--This code allows you to click on a song and pause a song thats already playing--!>
 	document.getElementById("badboy").pause();
 	document.getElementById("hypnotize").pause();
 	document.getElementById("missing").pause();
