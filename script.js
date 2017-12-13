7c03f2bb25cb3f9f011de5a1f5164479c0a13e87
DomoniqueS <stewartdomonique01@gmail.com>
2017-12-13 08:24:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 function playsong1(e){
 	document.getElementById("badboy").play();
-	document.getElementById("bigpoppa2").pause();
+	document.getElementById("poppa").pause();
 	document.getElementById("hypnotize").pause();
 	document.getElementById("missing").pause();
 	
 }
 function play2song(e){
-	document.getElementById("bigpoppa2").play();
+	document.getElementById("poppa").play();
 	document.getElementById("badboy").pause();
 	document.getElementById("hypnotize").pause();
 	document.getElementById("missing").pause();
 }
 function playsong3(e){
 	document.getElementById("hypnotize").play();
-	document.getElementById("bigpoppa2").pause();
+	document.getElementById("poppa").pause();
 	document.getElementById("badboy").pause();
 	document.getElementById("missing").pause();
 }
 function playsong4(e){
 	document.getElementById("missing").play();
 	document.getElementById("hypnotize").pause();
-	document.getElementById("bigpoppa2").pause();
+	document.getElementById("poppa").pause();
 	document.getElementById("badboy").pause();
 }
