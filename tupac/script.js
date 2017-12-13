78c98eee75527c1e408d9088a02ef585d19205ab
DomoniqueS <stewartdomonique01@gmail.com>
2017-12-13 08:28:46 -0800

    Add files via upload

---
+++ tupac/script.js
@@ -0,0 +1,56 @@
+
+function playsong1(e){
+	document.getElementById("badboy").play();
+	document.getElementById("bigpoppa2").pause();
+	document.getElementById("hypnotize").pause();
+	document.getElementById("missing").pause();
+	
+}
+function play2song(e){
+	document.getElementById("bigpoppa2").play();
+	document.getElementById("badboy").pause();
+	document.getElementById("hypnotize").pause();
+	document.getElementById("missing").pause();
+}
+function playsong3(e){
+	document.getElementById("hypnotize").play();
+	document.getElementById("bigpoppa2").pause();
+	document.getElementById("badboy").pause();
+	document.getElementById("missing").pause();
+}
+function playsong4(e){
+	document.getElementById("missing").play();
+	document.getElementById("hypnotize").pause();
+	document.getElementById("bigpoppa2").pause();
+	document.getElementById("badboy").pause();
+}
+function playsong5(e){
+	document.getElementById("aintmad").play();
+	document.getElementById("livediela").pause();
+	document.getElementById("baby").pause();
+	document.getElementById("headup").pause();
+}
+function playsong6(e){
+	document.getElementById("livediela").play();
+	document.getElementById("aintmad").pause();
+	document.getElementById("baby").pause();
+	document.getElementById("headup").pause();
+}
+function playsong7(e){
+	document.getElementById("headup").play();
+	document.getElementById("livediela").pause();
+	document.getElementById("aintmad").pause();
+	document.getElementById("doforlove").pause();
+	
+}
+function playsong8(e){
+	document.getElementById("baby").play();
+	document.getElementById("livediela").pause();
+	document.getElementById("aintmad").pause();
+	document.getElementById("doforlove").pause();
+	document.getElementById("headup").pause();
+}
+function notoriousmovie(e){
+	document.getElementById("notorious").play();
+	
+}
