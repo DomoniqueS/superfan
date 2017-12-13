57cf494be4247bd081deb01107c827546408f662
DomoniqueS <stewartdomonique01@gmail.com>
2017-12-13 08:13:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 !This function will allow the viewer to play music when they click on an image!
 function playsong1(e){
-  document.getElementById("poppa").play();
-  document.getElementById("bigpoppa").pause();
+  document.getElementById("badboy").play();
+  document.getElementById("poppa").pause();
   document.getElementById("momoney").pause();
 }
 function playsong2(e){
-  document.getElementById("bigpoppa").play();
+  document.getElementById("poppa").play();
   document.getElementById("badboy").pause();
   document.getElementById("hypnotize").pause();
   document.getElementById("momoney").pause();
 }
 function playsong3(e){
   document.getElementById("badboy").pause();
-  document.getElementById("bigpoppa").pause();
+  document.getElementById("poppa").pause();
   document.getElementById("momoney").pause();
 }
 function playsong4(e){
   document.getElementById("momoney").play();
   document.getElementById("badboy").pause();
-  document.getElementById("bigpoppa").pause();
+  document.getElementById("poppa").pause();
   document.getElementById("hypnotize").pause();
 }
